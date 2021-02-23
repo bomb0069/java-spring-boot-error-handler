f1f64497aff9ca871889627b4343337e62cc87fa
karan sivarat <bomb0069@gmail.com>
2021-02-23 15:27:53 +0700

    [Added]: Custom Exception for UserNotFoundException

--- src/main/java/pub/sck/java/springboot/example/ErrorHandler/solution2/User2Controller.java
+++ src/main/java/pub/sck/java/springboot/example/ErrorHandler/solution2/User2Controller.java
@@ -9,15 +9,15 @@ import pub.sck.java.springboot.example.ErrorHandler.solution1.ErrorResponse;
 public class User2Controller {
 
     @GetMapping("/users/{id}")
-    public User2Response getAllUser(@PathVariable int id) {
+    public User2Response getUser(@PathVariable int id) {
         if (id == 1)
             return new User2Response("Somebody No 1", 25);
-        throw new RuntimeException("User not found");
+        throw new UserNotFoundException();
     }
 
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(UserNotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    public Error2Response handleRuntimeException(RuntimeException exception) {
+    public Error2Response handleRuntimeException(UserNotFoundException exception) {
         return new Error2Response(exception.getMessage());
     }
 }
--- src/main/java/pub/sck/java/springboot/example/ErrorHandler/solution2/UserNotFoundException.java
+++ src/main/java/pub/sck/java/springboot/example/ErrorHandler/solution2/UserNotFoundException.java
@@ -1,6 +1,9 @@
 package pub.sck.java.springboot.example.ErrorHandler.solution2;
 
-public class UserNotFoundException extends Throwable {
-    public UserNotFoundException(int id) {
+public class UserNotFoundException extends RuntimeException {
+
+    @Override
+    public String getMessage(){
+        return "User not found";
     }
 }
