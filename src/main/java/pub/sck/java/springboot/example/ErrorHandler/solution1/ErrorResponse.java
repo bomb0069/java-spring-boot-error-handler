e7bc6f041461b3335e90f65f6d7850e6ccaa839c
karan sivarat <bomb0069@gmail.com>
2021-02-23 14:43:10 +0700

    [Added]: Solution 1 - ResponseEntity with Manual Response

---
+++ src/main/java/pub/sck/java/springboot/example/ErrorHandler/solution1/ErrorResponse.java
@@ -0,0 +1,17 @@
+package pub.sck.java.springboot.example.ErrorHandler.solution1;
+
+public class ErrorResponse {
+    private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/pub/sck/java/springboot/example/ErrorHandler/solution1/UserController.java
@@ -0,0 +1,27 @@
+package pub.sck.java.springboot.example.ErrorHandler.solution1;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@RequestMapping("/api/v1")
+public class UserController {
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity getAllUser(@PathVariable int id) {
+        try {
+            if (id == 1)
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new UserResponse("Somebody No 1", 25));
+            throw new RuntimeException("User not found");
+        } catch (RuntimeException exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(exception.getMessage()));
+        }
+    }
+}
