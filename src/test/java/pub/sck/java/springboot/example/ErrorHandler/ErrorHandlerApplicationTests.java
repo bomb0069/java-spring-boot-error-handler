7c5e0952ae7e24d4900cdd54c870921b7d02f624
karan sivarat <bomb0069@gmail.com>
2021-02-23 14:09:29 +0700

    [Added]: Initialized Project from start.spring.io

---
+++ src/test/java/pub/sck/java/springboot/example/ErrorHandler/ErrorHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package pub.sck.java.springboot.example.ErrorHandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
